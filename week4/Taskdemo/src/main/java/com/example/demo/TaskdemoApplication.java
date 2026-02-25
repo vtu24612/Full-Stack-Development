2c9fce0cf12e30aa340ab0806a7eafe6c0fe3a89
Madhan Kumar <vtu24612@veltech.edu.in>
2026-02-25 10:50:14 +0530

    week 4 annotations

---
+++ week4/Taskdemo/src/main/java/com/example/demo/TaskdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdemoApplication.class, args);
+	}
+
+}
---
+++ week4/Taskdemo/src/main/java/com/example/demo/constructor/PaymentService.java
@@ -0,0 +1,5 @@
+package com.example.demo.constructor;
+
+public interface PaymentService {
+	String pay();
+}
